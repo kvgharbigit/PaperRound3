4f58247f32322f05925eb237e190ff48073d9dfd
sohil27 <chhabrasohil@gmail.com>
2022-03-20 21:06:14 +1100

    admin

--- paper_round/lib/main.dart
+++ paper_round/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/jobs_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -22,7 +24,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 24, 2, 66),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
